4e8e262d786cc127f4776e20445dec6beed99adb
Kant <kant@mail.ru>
2022-09-28 14:42:25 +0300

    Сделал кулер и процессор

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import application from "../features/applicationSlice";
-import motherBoard from "../features/motherBoardSlice"
+import motherBoard from "../features/motherBoardSlice";
+import processor from '../features/processorSlice';
+import cooler from '../features/coolerSlice'
 
 export const store = configureStore({
   reducer: {
     application,
-    motherBoard
-  },
+    motherBoard,
+    processor,
+    cooler
+  }
 });
