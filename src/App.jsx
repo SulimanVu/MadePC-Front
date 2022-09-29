2bd5d6a8c3ee5f20e50365d2543b8255645bd716
Surkho <asa8964@mail.ru>
2022-09-29 15:03:36 +0300

    Сделал хеадер

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
 import DropDown from "./components/DropDown/DropDown";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return <div>
+    <Header />
     <DropDown />
     <Footer />
   </div>;
