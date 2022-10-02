c329280a83bfc64d2095f0ea2b7cf4f8d0b23261
Kant <kant@mail.ru>
2022-10-02 15:53:19 +0300

    Начало корзины

---
+++ src/pages/Basket/Basket.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../Basket/basket.module.scss'
+
+const Basket = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Basket;
