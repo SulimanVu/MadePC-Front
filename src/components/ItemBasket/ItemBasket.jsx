f62e07bc1ae7b8662a5c0ef85e5bc7e25182ae21
Kant <kant@mail.ru>
2022-10-04 23:08:18 +0300

    Сделал добавление в корзину, удаление пока не дается...

--- src/components/ItemBasket/ItemBasket.jsx
+++ src/components/ItemBasket/ItemBasket.jsx
@@ -3,12 +3,21 @@ import styles from '../ItemBasket/itembasket.module.scss';
 import proc from './images/proc2.png';
 import rub from '../CardComp/images/rub.svg';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../../features/applicationSlice';
+import { deleteFromBasket, fetchUsers } from '../../features/applicationSlice';
+import { fetchComp } from '../../features/compSlice';
 
 const ItemBasket = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.application.users)
+    const comps = useSelector((state) => state.comp.comp)
     const id = useSelector((state) => state.application.id)
+    console.log(comps);
+
+    const result = comps.map((element) => {
+        return element._id
+    })
+
+    console.log(result);
 
     const data = user.find((element) => {
         if (element._id === id) {
@@ -19,7 +28,13 @@ const ItemBasket = () => {
 
     useEffect(() => {
         dispatch(fetchUsers())
+        dispatch(fetchComp())
     }, [dispatch])
+
+    const handleDelete = (result1) => {
+
+        dispatch(deleteFromBasket({ result1, id }))
+    }
 
     return (
         <>
@@ -40,7 +55,7 @@ const ItemBasket = () => {
                                 <p>0</p>
                                 <button>+</button>
                             </div>
-                            <button className={styles.delete}>
+                            <button className={styles.delete} onClick={() => handleDelete(result)}>
                                 ╳
                             </button>
                         </div>
