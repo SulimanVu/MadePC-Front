619810941dde852dd1beeac7b78ebc5bc9a1c0a8
Saluev <ibragim.saluev@mail.ru>
2022-10-10 10:31:42 +0300

    Исправил faq

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,49 +1,33 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchQuestion } from "../../features/questionSlice";
+import { useState } from "react";
+import { motion } from "framer-motion";
 import styles from "../Faq/faq.module.scss";
 
-const Faq = () => {
+const Faq = ({ item, index }) => {
   const [secret, setSecret] = useState(false);
-
-  const question = useSelector((state) => state.questionSlice.question);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchQuestion());
-  }, [dispatch]);
 
   const handleClick = () => {
     setSecret(!secret);
   };
-
+  console.log(item);
   return (
-    <div className={styles.bodys}>
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className={styles.block}>
-        <h1 className={styles.Title}>Часто задаваемые вопросы</h1>
-        {question.map((item) => {
-          return (
-            <div className={styles.Text} onClick={handleClick}>
-              <div
-                className={styles.Numbers}
-                style={{ color: secret ? "#a7e200" : "#fff" }}
-              >
-                {item.nomer}. {item.vopros}
-                <button>{secret ? "-" : "+"}</button>
-              </div>
-              <div
-                className={styles.Secret}
-                style={{ display: secret ? "block" : "none" }}
-              >
-                {item.otvet}
-              </div>
-            </div>
-          );
-        })}
+    <div className={styles.bodys} key={index}>
+      <div className={styles.Text} onClick={handleClick}>
+        <div
+          className={styles.Numbers}
+          style={{ color: secret ? "#a7e200" : "#fff" }}
+        >
+          {item.nomer}. {item.vopros}
+          <button>{secret ? "-" : "+"}</button>
+        </div>
+        {secret ? <motion.div
+          transition={{ duration: 0.5 }}
+          initial={{ opacity: 0, y: -30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          className={styles.Secret}
+          // style={{ display: secret ? "block" : "none" }}
+        >
+          {item.otvet}
+        </motion.div> : null}
       </div>
     </div>
   );
